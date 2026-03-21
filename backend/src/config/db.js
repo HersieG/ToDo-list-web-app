f1b3073bd7296ca763080a30780822ce7e45c5c2
HersieG <hersiedgonzalez@gmail.com>
2026-03-21 14:32:05 -0400

    FEAT: added error handling for DB and connected DB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,29 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-    log:
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
 });
 
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+    process.exit(1);
+  }
+};
 
+async function disconnectDB() {
+  try {
+    await prisma.$disconnect();
+    console.log("Disconnected from the database");
+  } catch (error) {
+    console.error("Error disconnecting from the database:", error.message);
+  }
+}
 
-
-export default prisma;
+export { prisma, connectDB, disconnectDB };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
+import { connectDB, disconnectDB } from "./config/db.js";
 // routes
 import taskRoutes from "./routes/taskRoutes.js";
 
 config();
+connectDB();    
 
 const app = express();
 
@@ -11,11 +13,32 @@ app.use(express.json());
 app.use("/tasks", taskRoutes);
 
 const PORT = 5001;
-const server = app.listen(PORT, () => {
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// AUTH
-// USERS
-// TASKS
-// LIST
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err.message);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", async (err) => {
+    console.error("Uncaught Exception:", err.message);
+    await disconnectDB();
+    process.exit(1);
+});
+
+
+
+process.on("SIGTERM", (err) => {
+  console.error("SIGTERM received:", err ? err.message : "No error message");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
+});
